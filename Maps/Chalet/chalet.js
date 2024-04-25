2f2387e72b89500425792cc539e366806b53b277
Nolan Berg <nolanb912@gmail.com>
2024-04-25 02:12:53 -0600

    added chalet and clubhouse buttons and pages as well as cleaned us siege.css

---
+++ Maps/Chalet/chalet.js
@@ -0,0 +1,3 @@
+document.getElementById("mainMenuButton").addEventListener("click", function() {
+    window.location.href = "/siege.html"; // Go back to the main menu
+});
--- siege.js
+++ siege.js
@@ -1,4 +1,15 @@
-document.getElementById("newPageButton").addEventListener("click", function() {
-    window.location.href = "Bank/bank.html"; // Go to new page
+document.getElementById("bankPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Bank/bank.html"; // Go to bank page
 });
 
+document.getElementById("borderPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Border/border.html"; // Go to border page
+});
+
+document.getElementById("chaletPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Chalet/chalet.html"; // Go to chalet page
+});
+
+document.getElementById("clubhousePageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Clubhouse/clubhouse.html"; // Go to clubhouse page
+});
