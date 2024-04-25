7c4c1a1889a885d4d648c2f1145ce4b492bac316
Nolan Berg <nolanb912@gmail.com>
2024-04-24 21:24:00 -0600

    changing bank ui and adding border files

--- siege.js
+++ siege.js
@@ -1,4 +1,4 @@
 document.getElementById("newPageButton").addEventListener("click", function() {
-    window.location.href = "newpage.html"; // Go to new page
+    window.location.href = "Bank/bank.html"; // Go to new page
 });
 
