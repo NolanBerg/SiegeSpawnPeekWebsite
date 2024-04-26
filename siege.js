26048b51c7f40fc94ac09e2eb018af6384925cef
Nolan Berg <nolanb912@gmail.com>
2024-04-26 02:25:43 -0600

    put maps in scrollbar style on left side of menu as well as added buttons for all the previously missing maps

--- siege.js
+++ siege.js
@@ -17,3 +17,47 @@ document.getElementById("clubhousePageButton").addEventListener("click", functio
 document.getElementById("coastlinePageButton").addEventListener("click", function() {
     window.location.href = "Maps/Coastline/coastline.html"; // Go to coastline page
 });
+
+document.getElementById("consulatePageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Consulate/consulate.html"; // Go to consulate page
+});
+
+document.getElementById("emeraldplainsPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/EmeraldPlains/emeraldplains.html"; // Go to emerald plains page
+});
+
+document.getElementById("kafedostoyevskyPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/KafeDostoyevsky/kafedostoyevsky.html"; // Go to kafe dostoyevsky page
+});
+
+document.getElementById("kanalPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Kanal/kanal.html"; // Go to kanal page
+});
+
+document.getElementById("lairPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Lair/lair.html"; // Go to lair page
+});
+
+document.getElementById("nighthavenlabsPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/NighthavenLabs/nighthavenlabs.html"; // Go to nighthaven labs page
+});
+
+document.getElementById("oregonPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Oregon/oregon.html"; // Go to oregon page
+});
+
+document.getElementById("outbackPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Outback/outback.html"; // Go to outback page
+});
+
+document.getElementById("skyscraperPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Skyscraper/skyscraper.html"; // Go to skyscraper page
+});
+
+document.getElementById("themeparkPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/ThemePark/themepark.html"; // Go to theme park page
+});
+
+document.getElementById("villaPageButton").addEventListener("click", function() {
+    window.location.href = "Maps/Villa/villa.html"; // Go to villa page
+});
