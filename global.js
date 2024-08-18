0775ad608ab8770fb4aa34205579f6ec77d7d71d
Nolan Berg <nolanb912@gmail.com>
2024-08-17 21:47:43 -0600

    maps button

--- global.js
+++ global.js
@@ -2,3 +2,6 @@ document.getElementById("mainMenuButton").addEventListener("click", function() {
     window.location.href = "/index.html"; // Go back to the main menu
 });
 
+document.getElementById("mapsPageButton").addEventListener("click", function() {
+    window.location.href = "/index.html"; // Go back to the main menu
+});
