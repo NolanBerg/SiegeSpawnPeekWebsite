9bcb108d87ac1f46a01a491616c65b3b0c1f9939
Nolan Berg <nolanb912@gmail.com>
2024-09-16 00:37:56 -0600

    update bank to new ui

--- global.js
+++ global.js
@@ -1,15 +1,3 @@
 document.getElementById("mainMenuButton").addEventListener("click", function() {
     window.location.href = "/index.html"; // Go back to the main menu
 });
-
-document.getElementById("mapsPageButton").addEventListener("click", function() {
-    window.location.href = "/index.html"; // Go back to the main menu
-});
-
-document.getElementById("aboutPageButton").addEventListener("click", function() {
-    window.location.href = "/BannerLinks/about.html";
-});
-
-document.getElementById("contactPageButton").addEventListener("click", function() {
-    window.location.href = "/BannerLinks/contact.html";
-});
